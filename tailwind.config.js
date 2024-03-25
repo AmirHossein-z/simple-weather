6337615d7ef3c501eb39179f77f455eb5f7b87f1
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2024-03-25 17:45:49 +0330

    ci: add hokey-pokey color & two config for background-image

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,20 @@ export default {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
-            "neutral-gray": "#403B45",
-            primary: "#D2982D",
-            "primary-dark": "#DEA02E",
-            "primary-light": "#F5B747",
-            "secondary-dark": "#2B261D",
+            colors: {
+                "neutral-gray": "#403B45",
+                primary: "#D2982D",
+                "primary-dark": "#DEA02E",
+                "primary-light": "#F5B747",
+                "secondary-dark": "#2B261D",
+                "hokey-pokey": "#D69E36",
+            },
+            backgroundImage: {
+                "forecast-wave-mobile":
+                    "url('./assets/images/forecast-wave-mobile.svg')",
+                "forecast-wave-desktop":
+                    "url('./assets/images/forecast-wave-desktop.svg')",
+            },
         },
     },
     plugins: [],
